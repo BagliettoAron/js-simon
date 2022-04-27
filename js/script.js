8ee39e5b57fc2383ee36518777977a664fed645c
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-27 15:27:37 +0200

    Add: scaffolding & pseudo code in js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// 1 -- prelevo l'elemento dall'html su cui voglio appendere i numeri, 
+
+// - genero 5 numeri casuali (da 1 a 100) con la funzione mathrandom 
+// - pusho in un array chiamato originalNumbers
+// - appendo l'array all'elemento prelevato
+
+
+
+
+
+// 2 -- faccio partire un timer di 30 secondi alla fine del quale aggiungo la classe none al css riferito all'elemento in cui sono appesi i numeri generati, facendoli sparire
+// - nel contempo faccio comparire n5 prompt che chiedono all'utente di inserire i valori precedentemente visualizzati uno per volta
+// - inserisco i 5 valori forniti dall'utente in un array
+
+// 3 -- con la funzione includes cerco di capire quali e quanti numeri dell'array contenente le risposte fornite dall'utente coincidono con l'array originalNumbers
+
+
