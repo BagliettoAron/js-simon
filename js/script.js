882d9b3b0fa8eddadb24bdbd476f8134cb06a7ac
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-27 16:31:03 +0200

    Fix: const to start game in js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@
 let originalNumbersOutput = document.getElementById("provided-numbers");
 
 // - genero 5 numeri casuali (da 1 a 100) con la funzione mathrandom 
-lengthOriginalNumbers = 5;
-minStartOriginalNumbers = 1;
-maxEndOriginalNumbers = 100;
-timeoutShowNumbers = 5000;
+const lengthOriginalNumbers = 5;
+const minStartOriginalNumbers = 1;
+const maxEndOriginalNumbers = 100;
+const timeOutToStart = 5000;
 
 const originalNumbersArray = [];
 
@@ -25,13 +25,10 @@ originalNumbersOutput.append( "memorizza i seguenti numeri, hai 30 secondi: ",  
 
 
 // 2 -- faccio partire un timer di 30 secondi alla fine del quale aggiungo la classe none al css riferito all'elemento in cui sono appesi i numeri generati, facendoli sparire
-const countDown = setTimeout(timeOutHandler, timeoutShowNumbers);
+const countDown = setTimeout(timeOutHandler, timeOutToStart);
 console.log("countdown", countDown);
 
-function timeOutHandler() {
-    originalNumbersOutput.classList.add("d-none")
-    console.log(originalNumbersOutput);
-}
+
 // - nel contempo faccio comparire n5 prompt che chiedono all'utente di inserire i valori precedentemente visualizzati uno per volta
 // - inserisco i 5 valori forniti dall'utente in un array
 
@@ -42,4 +39,10 @@ function timeOutHandler() {
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
+
+
+function timeOutHandler() {
+    originalNumbersOutput.classList.add("d-none")
+    console.log(originalNumbersOutput);
+}
